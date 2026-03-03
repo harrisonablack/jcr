1c69b67b4b87c358c847abc7a67f97c006d94e0c
Harrison Black <dev+github@harrison.black>
2026-03-03 21:20:17 +1100

    Set empty colorscheme

--- lua/jcr.lua
+++ lua/jcr.lua
@@ -1,1 +1,16 @@
-print "jcr lua"
+local M = {}
+-- local theme = require('jcr.theme')
+
+M.setup = function()
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'jcr'
+
+  -- theme.set_highlights()
+end
+
+return M
